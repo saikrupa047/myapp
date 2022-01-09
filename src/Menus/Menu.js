d1a9444c1f7cb87fd39021ddfd9988851264a89b
madhav <76476279+draco-git@users.noreply.github.com>
2022-01-09 23:43:40 +0530

    Revert "Killer"

--- src/Menus/Menu.js
+++ src/Menus/Menu.js
@@ -1,23 +1,22 @@
-import React from "react";
-import "./Menu.css";
-import SideMenu from "../home/SideMenu";
+import React from 'react';
+import './Menu.css';
 
-class Menu extends React.Component {
-  menuClickHandler = (event) => {
-    console.log(event.target.value);
-    this.props.onSelect(event.target.value);
-  };
+class Menu extends React.Component
+{
+    menuClickHandler = (event) => {
+        console.log(event.target.value);
+        this.props.onSelect(event.target.value);
+    };
 
-  render() {
-    return (
-      // <div className='flex_container'>
-      //     <button onClick={this.menuClickHandler} value={this.props.title}>
-      //         {this.props.title}
-      //     </button>
-      //     </div>
-      <SideMenu></SideMenu>
-    );
-  }
+    render(){
+        return(
+            <div className='flex_container'>
+                <button onClick={this.menuClickHandler} value={this.props.title}>
+                    {this.props.title}
+                </button>
+                </div>
+        );
+    }
 }
 
 export default Menu;
